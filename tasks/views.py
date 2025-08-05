465eb51297980122e39fe6c46ebc6101db6dacd1
Morgan Parker <mparker2@uccs.edu>
2025-08-05 15:14:35 -0600

    Created template for tasks; current tasks may now be listed

--- tasks/views.py
+++ tasks/views.py
@@ -3,4 +3,4 @@ from .models import Task
 
 def task_list(request):
     tasks = Task.objects.all()
-    return render(request, 'tasks.task_list.html', {'tasks': tasks})
+    return render(request, 'tasks/task_list.html', {'tasks': tasks})
