e7c793ca66d0d5d5edda3f5983230c115eb661b8
Morgan Parker <mparker2@uccs.edu>
2025-08-07 19:54:40 -0600

    Added URL for updating a task

--- tasks/urls.py
+++ tasks/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.task_list, name='task_list'),
     path('create/', views.create_task, name='create_task'),
+    path('task/<int:pk>/edit/', views.update_task, name='update_task'),
 ]
