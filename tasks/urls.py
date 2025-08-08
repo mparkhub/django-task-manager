07638445675171de558b0f637ef19cd1742bbd7b
Morgan Parker <mparker2@uccs.edu>
2025-08-08 14:20:58 -0600

    Add URL for deleting task

--- tasks/urls.py
+++ tasks/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.task_list, name='task_list'),
     path('create/', views.create_task, name='create_task'),
     path('task/<int:pk>/edit/', views.update_task, name='update_task'),
+    path('task/<int:pk>/delete/', views.delete_task, name='delete_task'),
 ]
