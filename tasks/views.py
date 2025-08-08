ff363785013cc34c1df652d818c0cb551e9ff36e
Morgan Parker <mparker2@uccs.edu>
2025-08-08 14:24:49 -0600

    Add view for delete_task

--- tasks/views.py
+++ tasks/views.py
@@ -28,3 +28,12 @@ def update_task(request, pk):
         form = TaskForm(instance=task)
 
     return render(request, 'tasks/update_task.html', {'form':form, 'task':task})
+
+def delete_task(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+
+    if request.method == 'POST':
+        task.delete()
+        return redirect('task_list')
+    
+    return render(request, 'tasks/delete_task.html', {'task':task})
